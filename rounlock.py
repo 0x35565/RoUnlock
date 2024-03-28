820cfef44ea354bd15f6a9fd4edef26572603325
coolpancakes <73265375+coolpancakes@users.noreply.github.com>
2024-03-28 19:24:42 +1100

    Update rounlock.py

--- rounlock.py
+++ rounlock.py
@@ -14,14 +14,14 @@ def add_json():
     potential_targets = []
     for rblx in rblx_dir.iterdir():
 	potential_targets.append(rblx)
-    for potential_target in potential_targets:
-	for potential_target_inside_dir in potential_target.iterdir():
-	    if 'RobloxPlayerBeta.exe' in str(potential_target_inside_dir):
-	    os.chdir(str(potential_target_inside_dir).strip(r'\RobloxPlayerBeta.exe'))
-	    os.system('mkdir ClientSettings') 
-            os.chdir('ClientSettings')
-	    with open('ClientAppSettings.json', 'w') as fps:
-		fps.write('{"DFIntTaskSchedulerTargetFps": 101010}')
+    	for potential_target in potential_targets:
+	    for potential_target_inside_dir in potential_target.iterdir():
+	        if 'RobloxPlayerBeta.exe' in str(potential_target_inside_dir):
+	        os.chdir(str(potential_target_inside_dir).strip(r'\RobloxPlayerBeta.exe'))
+	        os.system('mkdir ClientSettings') 
+                os.chdir('ClientSettings')
+	        with open('ClientAppSettings.json', 'w') as fps:
+		    fps.write('{"DFIntTaskSchedulerTargetFps": 101010}')
 					
 
 add_json()
