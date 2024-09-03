dadcb5b19ccd4813e43521d1fdfd6b625abff708
0x4155 <73265375+0x4155@users.noreply.github.com>
2024-09-03 10:39:54 +1000

    Update rounlock.py

--- rounlock.py
+++ rounlock.py
@@ -9,7 +9,7 @@ from pathlib import Path
 def banner():
     print(r'''
 
-			made by coolpancakes | RoUnlock | https://github.com/coolpancakes
+			made by 0x4155 | RoUnlock | https://github.com/coolpancakes
 
 
         ____        __  __      __           __  
